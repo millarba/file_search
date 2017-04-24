87daeb5f25fcd49d01e82d419aef99ac3a7ac551
millarba <millarba@gmail.com>
2017-04-24 13:25:19 -0500

    cleaned up investors

--- runner.rb
+++ runner.rb
@@ -4,7 +4,7 @@ file = File.open("file.txt", "r")
 
 contents = file.read
 @investors = []
-  investor_counter = 1
+investor_counter = 1
 
 puts "Investors"
 puts "---------------"
@@ -12,14 +12,13 @@ contents.each_line do |line|
   break if line.chomp.split(":") == []
   input = line.chomp.split(",")
   investor = Investor.new({first_name: input[0], last_name: input[1], id: input[2]})
-  if investor.first_name != ":Investors:"
-    if investor.first_name != "First Name"
-      puts "#{investor_counter} - #{investor.first_name} #{investor.last_name}, id: #{investor.id}"
-      investor_counter += 1
-    end
-  end
+  @investors << investor
 end
 
+@investors[2..100].each do |investor|
+  puts "#{investor_counter} - #{investor.first_name} #{investor.last_name}, id: #{investor.id}"
+  investor_counter += 1
+end
 
 
 
